ce6729d9d40e8bb05a292458ec1705e0a87358b8
Giulianna Ribeiro <giuliannaribeiro00@gmail.com>
2023-05-08 21:34:20 -0300

    Update nova-transferencia.component.ts

--- nova-transferencia.component.ts
+++ nova-transferencia.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'], 
 })
 export class NovaTransferenciaComponent {
-  valor: number; //propriedade valor
+  valor: number; 
   destino: number;
 
   transferir() {
